c18015f63810bec81010f3b5012a0e6efb6be40c
Edwin Serna <sernaedwin0@gmail.com>
2026-05-09 15:10:57 -0500

    Added colored trails for better visalization of robot states when traveling.

--- source/CPFA/CPFA_controller.cpp
+++ source/CPFA/CPFA_controller.cpp
@@ -99,25 +99,23 @@ void CPFA_controller::ControlStep() {
 	switch(CPFA_state) {
 		case DEPARTING: 
 		  m_pcLEDs->SetAllColors(CColor::BLUE); // departing state is blue
+		  TrailColor = CColor::BLUE;
 			break;
 		case SEARCHING: // searching state is green
 			m_pcLEDs->SetAllColors(CColor::GREEN);
+			TrailColor = CColor::GREEN;
 			break;
 		case RETURNING: // returning state is red
 			m_pcLEDs->SetAllColors(CColor::RED);
+			TrailColor = CColor::RED;
 			break;
 		case SURVEYING: // surveying state is yellow
 			m_pcLEDs->SetAllColors(CColor::YELLOW);
-			break;
-		case SHARING: // sharing state is cyan (flashing)
-			if((SimulationTick() % 10) < 5) {
-				m_pcLEDs->SetAllColors(CColor::CYAN);
-			} else {
-				m_pcLEDs->SetAllColors(CColor::BLACK);
-			}
+			TrailColor = CColor::YELLOW;
 			break;
 		default: // default trail color is orange
 			m_pcLEDs->SetAllColors(CColor::ORANGE);
+			TrailColor = CColor::ORANGE;
 	}
 
 	// Add line so we can draw the trail at robot height
